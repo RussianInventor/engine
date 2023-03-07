81ea17a2b89e5e8d62a848cd462fb0dc90e36e4e
eksarh <perm.mikh@gmail.com>
2023-03-07 20:41:29 +0300

    frame switching

--- client/interface.py
+++ client/interface.py
@@ -16,32 +16,35 @@ class InterApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.connectButton.clicked.connect(self.connect)
+        self.create_button_2.clicked.connect(lambda: self.show_frame('world_frame'))
         self.client = client
 
     def connect(self):
         host = self.hostEntry.text()
         port = int(self.portEntry.text())
         if self.client.connect(host, port, Config.id, Config.port):
-            self.show_idle_frame()
+            self.show_frame('idle_frame')
             self.client.set_state(IdleState)
         else:
             QtWidgets.QMessageBox(self, text="Проверьте адрес сервера и порт").show()
 
-    def show_connection_frame(self):
-        self.idle_frame.hide()
-        self.
-        self.connection_frame.show()
+    def show_frame(self, frame_name, on_load=None):
+        if on_load is not None:
+            on_load()
+        for frame in filter(lambda w: isinstance(w, QtWidgets.QFrame), self.centralwidget.children()):
+            if frame.objectName() == frame_name:
+                frame.show()
+            else:
+                frame.hide()
 
-    def show_idle_frame(self):
-        self.connection_frame.hide()
+    def load_worlds(self):
         for world in self.client.state.execute("get_world"):
             self.world_selection.addItem(text=world.name, userData=world.id)
-        self.idle_frame.show()
 
 
 def run_interface(client):
     app = QtWidgets.QApplication(sys.argv)
     window = InterApp(client)
     window.show()
-    window.show_connection_frame()
+    window.show_frame('connection_frame')
     app.exec_()
